a282a91d32dd698b4ecf3208b98746d8fe144c8e
Vishal Ratna <vratna@microsoft.com>
2023-01-22 12:20:58 +0530

    Producer consument class using volatile

--- src/main/java/org/example/PCExample.java
+++ src/main/java/org/example/PCExample.java
@@ -3,16 +3,20 @@ package org.example;
 public class PCExample {
 
     private volatile boolean shouldProduce = true;
+    private int value = -1;
 
     public void startProducer() {
         Runnable producer = () -> {
-            for (; ; ) {
+            for (int i = 0; i < 10; i++) {
                 while (!shouldProduce) { }
-                System.out.println("Produced");
+                System.out.println("Produced: " + i);
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
+                }
+                if (value == -1) {
+                    value = i;
                 }
                 shouldProduce = false;
             }
@@ -22,14 +26,15 @@ public class PCExample {
 
     public void startConsumer() {
         Runnable consumer = () -> {
-            for (; ; ) {
+            for (int i = 0; i < 10; i++) {
                 while (shouldProduce) { }
-                System.out.println("Consumed");
+                System.out.println("Consumed: " + value);
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+                value = -1;
                 shouldProduce = true;
             }
         };
