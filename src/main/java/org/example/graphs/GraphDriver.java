09ac1c9c0a9f9c5bbaaf0537e261a38dc696f4d4
Vishal Ratna <vratna@microsoft.com>
2023-02-21 21:44:41 +0530

    Add driver code

--- src/main/java/org/example/graphs/GraphDriver.java
+++ src/main/java/org/example/graphs/GraphDriver.java
@@ -1,8 +1,7 @@
 package org.example.graphs;
 
 
-import org.example.disjointset.DisjointSet;
-import org.example.graphs.base.Graph;
+import org.example.graphs.problems.ProblemSpanningTreeWithKruskal;
 
 public class GraphDriver {
 
@@ -21,18 +20,19 @@ public class GraphDriver {
 //        GraphUtils.printSetOfEdges(result.contributingEdges);
 //        System.out.println("Sum: " + result.sum);
 
-        Graph<Integer> graph = Graphs.create(Graphs.Type.UNDIRECTED);
-        for (int i = 1; i <= 7; i++) {
-            graph.addNode(i);
-        }
-        DisjointSet<Integer> set = new DisjointSet<>(graph.nodes());
-        set.hasSameRepresentative(1,2);
-        set.unionByRank(1, 2);
-        set.unionByRank(2, 3);
-        set.unionByRank(4, 5);
-        set.unionByRank(6, 7);
-        set.unionByRank(5, 6);
-        set.unionByRank(3, 7);
-        set.hasSameRepresentative(1,6);
+//        Graph<Integer> graph = Graphs.create(Graphs.Type.UNDIRECTED);
+//        for (int i = 1; i <= 7; i++) {
+//            graph.addNode(i);
+//        }
+//        DisjointSet<Integer> set = new DisjointSet<>(graph.nodes());
+//        set.hasSameRepresentative(1,2);
+//        set.unionByRank(1, 2);
+//        set.unionByRank(2, 3);
+//        set.unionByRank(4, 5);
+//        set.unionByRank(6, 7);
+//        set.unionByRank(5, 6);
+//        set.unionByRank(3, 7);
+//        set.hasSameRepresentative(1,6);
+
     }
 }
